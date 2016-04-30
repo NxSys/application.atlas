003fdbbd677d4a4a8642d6647973d81792543f2c
nxs.cfeamster <cfeamster@nxsysts.com>
2016-04-30 20:38:47 +0000

    Added codeception test skel

---
+++ tests/functional/MyTest.php
@@ -0,0 +1,24 @@
+<?php
+
+
+class MyTest extends \Codeception\TestCase\Test
+{
+    /**
+     * @var \FunctionalTester
+     */
+    protected $tester;
+
+    protected function _before()
+    {
+    }
+
+    protected function _after()
+    {
+    }
+
+    // tests
+    public function testMe()
+    {
+
+    }
+}
