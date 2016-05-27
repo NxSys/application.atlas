c13028fb83550f1ddbee69114e28accb6728527c
nxs.cfeamster <cfeamster@nxsysts.com>
2016-05-27 21:11:09 +0000

    Examine is now functional

--- src/NxSys/Applications/Atlas/Web/Controllers/Examine.php
+++ src/NxSys/Applications/Atlas/Web/Controllers/Examine.php
@@ -1,9 +1,17 @@
 <?php
 
 namespace NxSys\Applications\Atlas\Web\Controllers;
+
+use Silex\Application as WebApp;
 use Symfony\Component\HttpFoundation as SfHttp;
+
+use Webcreate\Vcs;
 
 class Examine
 {
-	
+	public function index(WebApp $oApp, SfHttp\Request $oRequest)
+	{
+		return "I'm Here!";
+		
+	}
 }
