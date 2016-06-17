22f103aa62d75d793182c1b77af95f47646e5d54
wgraber <kal.zekdor@gmail.com>
2016-06-17 21:02:58 +0000

    * Added Elastic Search Mapping Configuration

--- src/NxSys/Applications/Atlas/Services/Search.php
+++ src/NxSys/Applications/Atlas/Services/Search.php
@@ -4,6 +4,7 @@ namespace NxSys\Applications\Atlas\Services;
 
 use Silex\Application as WebApp;
 use Elastica\Client as SearchClient;
+use Elastica\Type\Mapping as SearchMapping;
 use Elastica\Exception\Connection\HTTPException;
 
 class Search
@@ -12,6 +13,15 @@ class Search
 	{
 		$this->app = $oApp;
 		$this->client = $oClient;
+		if ($this->checkStatus())
+		{
+			$sIndexName = $this->app['config']['search']['index'];
+			$this->index = $this->client->getIndex($sIndexName);
+			if (!$this->index->exists())
+			{
+				$this->setupIndex();
+			}
+		}
 	}
 	
 	public function checkStatus()
@@ -27,5 +37,42 @@ class Search
 		return True;
 	}
 	
+	/**
+	 * Creates and configures an ElasticSearch Index
+	 */
+	public function setupIndex()
+	{
+		$this->index->create();
+		
+		$oProjectType = $this->index->getType('project');
+		$oProjectType->setMapping( new SearchMapping($oProjectType,
+											 ['name' => 'string',
+											  'description' => 'string']));
+		
+		$oRepositoryType = $this->index->getType('repository');
+		$oRepositoryType->setMapping( new SearchMapping($oRepositoryType,
+														['location' => 'string',
+														 '_parent' => $oProjectType]));
+		
+		$oCommitType = $this->index->getType('commit');
+		$oCommitType->setMapping( new SearchMapping($oCommitType,
+											['revision' => 'string',
+											 'date' => 'date',
+											 'message' => 'message',
+											 '_parent' => $oRepositoryType]));
+		
+		$oFileType = $this->index->getType('file');
+		$oFileType->setMapping( new SearchMapping($oFileType,
+										  ['path' => 'string',
+										   'contents' => 'string',
+										   '_parent' => $oCommitType]));
+		
+		
+		$oDeveloperType = $this->index->getType('developer');
+		$oDeveloperType->setMapping( new SearchMapping($oDeveloperType,
+											   ['name' => 'string',
+												'user' => 'string',
+												'_parent' => $oCommitType]));
+	}
 	
 }
